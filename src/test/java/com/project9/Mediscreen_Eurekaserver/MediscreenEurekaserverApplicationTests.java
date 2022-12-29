d90a6ef9057427b4325b79e35d6d838e8f622232
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-29 22:52:28 +0100

    First commit: Eureka server to tie Microservices (Eureka Clients)

---
+++ src/test/java/com/project9/Mediscreen_Eurekaserver/MediscreenEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_Eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
